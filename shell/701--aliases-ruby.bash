4f965992b3a9729268c850bb516910a483fea7c4
Michael Barany <michael.barany@gmail.com>
2018-04-16 18:07:47 -0400

    Add ruby aliases

---
+++ shell/701--aliases-ruby.bash
@@ -0,0 +1,19 @@
+function rspec {
+  bundle exec rspec "$@"
+}
+
+function rake {
+  bundle exec rake "$@"
+}
+
+function rails {
+  bundle exec rails "$@"
+}
+
+function clockwork {
+  bundle exec clockwork "$@"
+}
+
+function sidekiq {
+  bundle exec sidekiq "$@"
+}
