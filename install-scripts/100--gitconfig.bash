314abc2b606a8addf1f3d02829171c318b165860
Michael Barany <michael.barany@gmail.com>
2018-04-17 17:32:43 -0400

    Prompt for name in gitconfig setup script

--- install-scripts/100--gitconfig.bash
+++ install-scripts/100--gitconfig.bash
@@ -1,4 +1,19 @@
 if [ ! -f $HOME/.gitconfig ]; then
-    git config --global include.path "$DOTFILES_DIR/.gitconfig"
-    git config --global core.excludesfile "$DOTFILES_DIR/.gitignore_global"
+  echo "Configuring ~/.gitconfig ..."
+
+  git config --global include.path "$DOTFILES_DIR/.gitconfig"
+  git config --global core.excludesfile "$DOTFILES_DIR/.gitignore_global"
+
+  if [ "$DOTFILES_OS" == "Darwin" ]; then
+    FULL_NAME=$(id -F)
+  else
+    FULL_NAME=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f 1)
+  fi
+  read -p "Full Name [$FULL_NAME]: " RESPONSE
+  RESPONSE=${RESPONSE:-$FULL_NAME}
+
+  git config --global user.name "$RESPONSE"
+
+  unset FULL_NAME
+  unset RESPONSE
 fi
