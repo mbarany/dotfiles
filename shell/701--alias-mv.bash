c7a1eefcd3632ff41bf65db31fb99546a8c1489e
Michael Barany <michael.barany@gmail.com>
2020-04-19 14:48:19 -0400

    Add mv

---
+++ shell/701--alias-mv.bash
@@ -0,0 +1,11 @@
+# https://gist.github.com/premek/6e70446cfc913d3c929d7cdbfe896fef
+
+function mv() {
+  if [ "$#" -ne 1 ] || [ ! -e "$1" ]; then
+    command mv "$@"
+    return
+  fi
+
+  read -ei "$1" newfilename
+  command mv -v -- "$1" "$newfilename"
+}
