79cc83b8982c894d28a89135d7d501c74051b5c3
Michael Barany <1434605+mbarany@users.noreply.github.com>
2020-10-28 16:10:27 -0400

    Add guard for known hosts file

--- bash_completion.d/ssh.bash
+++ bash_completion.d/ssh.bash
@@ -1,2 +1,5 @@
 # Take from ~/.ssh/known_hosts
-complete -W "$(echo `cat $HOME/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+
+if [ -f ${HOME}/.ssh/known_hosts ]; then
+  complete -W "$(echo `cat ${HOME}/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+fi
