6b2d00d90737c01271a38cd1aae81e8b72bda108
Michael Barany <1434605+mbarany@users.noreply.github.com>
2025-10-20 10:05:32 -0400

    Add nvm; remove composer

--- shell/990--bash_path.bash
+++ shell/990--bash_path.bash
@@ -2,8 +2,6 @@
 
 [[ "$GOPATH" != "" && -d ${GOPATH}/bin ]] && PATH="${GOPATH}/bin:${PATH}"
 
-[[ -d ${HOME}/.composer/vendor/bin ]] && PATH="${HOME}/.composer/vendor/bin:${PATH}"
-
 [[ -d ${DOTFILES_DIR}/bin ]] && PATH="${DOTFILES_DIR}/bin:${PATH}"
 
 [[ -d ${HOME}/.local/bin ]] && PATH="${HOME}/.local/bin:${PATH}"
